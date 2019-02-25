f9d540251139fe4f1bff118d4ed44777d4d52f67
Allen Chang <achang24678@gmail.com>
2019-02-24 23:48:39 -0800

    Create generateMessage utils

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const http = require('http');   // it's a built in node module, no need to insta
 const express = require('express');
 const socketIO = require('socket.io');
 
+var {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();  //create new express app
 var server = http.createServer(app);
 var io = socketIO(server);  // we get our web sockets server on here we can emitting or listening to events (communicate between server and client)
+
+
 
 app.use(express.static(publicPath));  //configure our middleware
 
@@ -15,26 +18,16 @@ app.use(express.static(publicPath));  //configure our middleware
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+// when user logon to the server, they gonna see this message first
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  // user who log on to the chat app won't see this block of message, but other who already logged on will see new user joined
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message) => {    //listener, listen for event and fire back
     console.log('createMessage', message);
     //io.emit emits an event to every single connection here (when we get one user creating message and send to server, server sends back to all connections of this server)
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
     //this lets the socket IO library know which user shouldn't get the event (send to everybody but this socket, user who fire out the message won't see the message like welcome Allen)
     // socket.broadcast.emit('newMessage', {
